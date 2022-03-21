80981fd2fb4d35954b492b6f68d3a0b9aef6dbe4
vishnuvenkatp <102011848+vishnuvenkatp@users.noreply.github.com>
2022-03-22 00:19:23 +0530

    employees schema

--- employees/employees.controller.ts
+++ employees/employees.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, Request, Res, Response } from '@nestjs/common';
+import { resolve } from 'path';
+import { EmployeesService } from './employees.service';
 
 @Controller('employees')
-export class EmployeesController {}
+export class EmployeesController {
+    constructor(
+        private readonly employeeService: EmployeesService
+    )
+    {}
+
+//    @Post('createEmployee')
+//     async create{@Body() body:any ,@Res() res:Response,@Req() req:Request){
+
+//     }
+
+    }
+
--- employees/schema/employees.schema.ts
+++ employees/schema/employees.schema.ts
@@ -3,6 +3,27 @@ import * as mongoose from 'mongoose';
 
 export const employeeInfo = new mongoose.Schema({
 
+    employeeId:{type :Number},
+    employeeName:{type:String},
+    phoneNumber:{type :Number},
+    emailId:{type:String},
+    employeeAddress:[
+        {
+            address:{type:String},
+            city:{type:String},
+            pincode:{type :Number},
+        }
+    ],
+    role:{type:String},
+    projects:{type:String},
+    expierence:{type :Number},
+    skills:[
+        {
+            domain:{type:String},
+            years:{type :Number},
+        }
+    ]
+
 },
 {
     versionKey: false,
